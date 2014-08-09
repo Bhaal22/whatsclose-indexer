40299b74b38a64aaabaeca73ff745e3d89ec2358
Bhaal22 <muller.john@gmail.com>
2014-08-09 16:41:40 +0200

    -dedicated indexer for concert type mapping -test another line

---
+++ indexer.es/Concert.Indexer.js
@@ -0,0 +1,40 @@
+var winston = require('winston');
+var es = require('elasticsearch');
+
+function Concert.Indexer () {
+  this.index = 'whatsclose';
+  this.type = 'concert';
+  this.es_client = new es.Client ({
+    host: 'localhost:9200',
+    port: '9200'
+  });
+}
+
+Indexer.prototype.exists = function (data) {
+  throw 'Not implemented';
+}
+
+Indexer.prototype.publish = function (data) {
+
+  if (!this.exists (data)) {
+    this.es_client.create({
+      index: this.index,
+      type: this.type,
+      body: data
+    }, function (error, response) {
+      if (error) {
+        winston.error("NOT COOL! [" + error + "]");
+      } else {
+        if (response) {
+          winston.log(response);
+        } else {
+          winston.log("vas savoir ^^");
+        }
+      }
+    });
+  }
+}
+
+module.exports = {
+  I: Indexer
+};
