f2cc972ff353f832185a033939a4e054c30d58b9
Bhaal22 <muller.john@gmail.com>
2014-08-12 08:50:43 +0200

    ## added argument parser

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
 var winston = require('winston');
 var eventEmitter = require('./services/CustomEventEmitter');
 var svc_handler = require('./services/ServicesHandler.js');
+var argv = require('yargs').argv;
 
 var init_logger = function () { 
-  //winston.add(winston.transports.File, { filename: 'whatsclose.log' });
-  //winston.remove(winston.transports.Console);
+
   var custom_log_levels = {
     colors: {
       log: 'white',
@@ -18,9 +17,23 @@ var init_logger = function () {
 };
 
 init_logger ();
-// Services initialization
-svc_handler.init(winston);
 
-// Entry Point for indexation
-eventEmitter.emit("crawlData");
+var show_help = function () {
+  console.log ("--- Whatsclose indexer help ---");
+};
+
+
+
+if (argv.help) {
+  show_help ();
+} else if (argv.show_events) {
+  dump_event_description ();
+} else {
+  
+  // Services initialization
+  svc_handler.init(winston);
+  
+  // Entry Point for indexation
+  eventEmitter.emit("crawlData");
+}
 
