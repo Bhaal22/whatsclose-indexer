276a059519306c5b9d9a57bd0addbd79a0142960
Bhaal22 <muller.john@gmail.com>
2014-09-07 15:37:33 +0200

    #26 first attempt with GraphApi service

--- services/GraphApiCrawler.js
+++ services/GraphApiCrawler.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var http = require('http');
 var env = require('jsdom').env;
 var Q = require('q');
 var winston = require('./CustomWinston.js');
@@ -13,12 +14,40 @@ var CRAWLED_EVENT = "crawled";
 var CrawlService = function() {
 	this.crawl_modules = [];
 	this.moduleName = "CrawlService";
+
+  this.access_token = "";
+};
+
+var auth = function () {
+  //https://graph.facebook.com/oauth/access_token?client_id=699899433422062&client_secret=4a9c3c206928726923400b1073c6df56&grant_type=client_credentials
+  
+  var endpoint = {
+    hostname: 'www.google.com',
+    port: 443,
+    path: '/oauth/access_token',
+    method: 'GET' };
+  
+  var request = http.request(options, function(res) {
+    
+    var data = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      data += chunk
+    });
+
+    res.on('end', function () {
+      console.log (data);
+    });
+  });
+
 };
 
 CrawlService.prototype.init = function() {
 	var self = this;
-	// retrieve the crawlers js files
-	var crawlersDir = fs.readdirSync('./crawlers/web');
+
+  auth ();
+  
+	var crawlersDir = fs.readdirSync('./crawlers/graphApi');
 
 	for ( var i = 0, ii = crawlersDir.length; i < ii; i++) {
     var band_file_name = crawlersDir[i];
@@ -27,10 +56,10 @@ CrawlService.prototype.init = function() {
     var match = band_file_name.match(regex);
 
     if (match) {
-      
+
 		  winston.info('Crawler file found : ' + band_file_name);
 		  // Load the js files as node modules
-		  var module = require('../crawlers/'
+		  var module = require('../crawlers/graphApi'
 				                   + band_file_name.replace(/.js$/, ""));
 		  this.crawl_modules.push(module);
     }
