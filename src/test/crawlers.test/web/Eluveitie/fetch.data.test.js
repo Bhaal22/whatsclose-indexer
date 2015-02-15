b3af1916724be7f7795c1a5c563d80ef71d89ac9
Bhaal22 <muller.john@gmail.com>
2015-02-15 22:42:00 +0100

    moved eluveitie to prod

--- src/test/crawlers.test/web/Eluveitie/fetch.data.test.js
+++ src/test/crawlers.test/web/Eluveitie/fetch.data.test.js
@@ -2,7 +2,7 @@ var request = require('superagent');
 var expect = require('expect.js');
 
 global.__base = __dirname + '/../../../../';
-var module = require(__base +  'crawlers/web/incubation/Eluveitie').crawlModule;
+var module = require(__base +  'crawlers/web/Eluveitie').crawlModule;
 
 describe('Eluveitie Suite', function(){
   it('get Eluveitie Tour generation', function(done){
