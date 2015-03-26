6091a2c22e22591d2c35cd6444be5ca0951ffa85
Bhaal22 <muller.john@gmail.com>
2015-03-26 23:46:27 +0100

    added nightwish as a band to crawl

---
+++ src/crawlers/web/incubation/nightwish.js
@@ -0,0 +1,60 @@
+var Band = require('../../model/Band');
+var CrawlerModule = require('../../model/CrawlerModule');
+var winston = require('winston');
+require('datejs');
+
+var jquery = require('jquery');
+
+var _module = new CrawlerModule();
+_module.fullUrl = 'http://nightwish.com/en/live';
+_module.band = new Band();
+_module.band.name = 'Nightwish';
+_module.band.website = 'http://nightwish.com';
+
+_module.band.styles = ['Symphonic Metal'];
+
+// Override the method that assess the web page structure
+_module.testDataAcess = function() {
+  winston.info('Dark Tranquility testDataAcess');
+  return true;
+};
+
+_module.date = function (d) {
+  var date_return;
+  try {
+    date_return = Date.parseExact(d, ['yyyy-MM-dd']);
+  } catch(e) {
+    console.log(e);
+  }
+  return date_return.toString('yyyy-MM-dd');
+}
+
+// Override the method that retrieve the events data
+_module.processData = function(window) {
+  winston.info('dark tranquility processDate');
+
+  var $ = require('jquery')(window);
+  
+  var results = [];
+  var rows = $ ('.events-table > table > tr');
+
+  var self = this;
+
+  console.log('DT entries: ', rows.length - 1);
+  rows.slice(1).each (function (index) {
+    var infos = $('td', this);
+    
+    var date = $('td.date > meta', this).attr('content');
+    var venue = $(infos[1]).text().trim();
+    var location = $(infos[2]).text().trim();
+
+    results.push({ date: self.date (date), venue: venue, location: location });
+  });
+
+  return results;
+}
+
+
+module.exports = {
+  crawlModule: _module
+};
