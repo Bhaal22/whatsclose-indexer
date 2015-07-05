aa4bc1df0820b7a7172df58f3b651d2ea5fda3bd
Bhaal22 <muller.john@gmail.com>
2015-07-05 23:15:13 +0200

    added Korn module

---
+++ src/crawlers/web/incubation/korn.js
@@ -0,0 +1,57 @@
+var Band = require(__base + 'model/Band');
+var CrawlerModule = require(__base + 'model/CrawlerModule');
+var winston = require('winston');
+require('datejs');
+
+
+var newCrawlModule = new CrawlerModule();
+newCrawlModule.fullUrl = 'http://www.korn.com/events/';
+newCrawlModule.band = new Band();
+newCrawlModule.band.name = 'Korn';
+newCrawlModule.band.website = 'http://www.korn.com';
+
+newCrawlModule.band.styles = [ "Punk hardcore", "Crossover thrash", "Digital hardcore", "Metalcore" ]
+
+// Override the method that assess the web page structure
+newCrawlModule.testDataAcess = function() {
+  winston.info('tagada jones testDataAcess');
+  return true;
+};
+
+// Override the method that retrieve the events data
+newCrawlModule.processData = function(window) {
+
+ winston.info('tagada jones processDate');
+
+  var $ = require('jquery')(window);
+  var dates_table = $('table.dates_list');
+	var rows = $ ('#scroll-content-dates > ul > li');
+	
+  var results = [];
+
+  console.log('tagada jones entries: ', rows.length);
+  rows.each (function (index) {
+    var date = $('span.DateDate', this).text ();
+    var fullLocation = $(this).text ();
+    var location = fullLocation.split ('\n')[2].trim ();
+    var venue = fullLocation.split ('\n')[4].trim ();
+    var dateElements = date.split ("/");
+		
+		results.push({date: new Date (dateElements[2], dateElements[1]-1, dateElements[0]).toString('yyyy-MM-dd'), 
+			      location: location,
+			      venue: venue
+			     });
+
+  });
+
+  return results;
+};
+
+
+module.exports = {
+  crawlModule: newCrawlModule
+};
+
+
+
+
---
+++ src/test/crawlers.test/web/korn/fetch.data.test.js
@@ -0,0 +1,25 @@
+var request = require('superagent');
+var expect = require('expect.js');
+
+global.__base = __dirname + '/../../../../';
+
+var tagada = require(__base + 'crawlers/web/incubation/korn').crawlModule;
+
+describe('Tagada Jones Suite', function(){
+  it('get Tagada Jones Tour generation', function(done) {
+
+    tagada.crawlWebData().
+      then (function (data) {
+        var concerts = tagada.band.concerts;
+
+        console.dir(concerts);
+
+        concerts.forEach(function(concert) {
+          console.log(concert.venue);
+        });
+        expect(concerts).to.not.be.empty();
+
+        done ();
+      });
+  });  
+});
