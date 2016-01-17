2cec87dab2d559c809951f32f4de315c2228f695
unknown <jmuller@cnjmuller.ent.gsx.com>
2016-01-17 21:47:39 +0100

    #16: fixed A Day to Remember Unit Tests

--- src/test/crawlers.test/web/a_day_to_remember/fetch.data.test.js
+++ src/test/crawlers.test/web/a_day_to_remember/fetch.data.test.js
@@ -35,15 +35,21 @@ describe('A Day To Remember Suite', function(){
     
     var date = bandModule.date ("APR 4");
 
-    expect(date).to.equal('2015-04-04');
+    var now = new Date();
+    
+    expect(date).to.equal(1900+now.getYear() + '-04-04');
     done();
   });
 
   it('A Day To Remember special dates', function(done){
     
     var date = bandModule.date ("Mar 15");
+    var now = new Date();
 
-    expect(date).to.equal('2015-03-15');
+    var currentYear = now.getYear() + 1900
+    
+    expect(date).to.equal(currentYear + '-03-15');
     done();
   });
 });
+
