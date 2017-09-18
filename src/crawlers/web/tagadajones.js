b2f57745e3a298e326f5de3940f5da6646fdbaed
Bhaal22 <muller.john@gmail.com>
2017-09-19 01:09:16 +0200

    fixed tagada jones web site update

--- src/crawlers/web/tagadajones.js
+++ src/crawlers/web/tagadajones.js
@@ -5,51 +5,53 @@ require('datejs');
 
 
 var newCrawlModule = new CrawlerModule();
-newCrawlModule.fullUrl = 'http://www.tagadajones.com/new/#/Dates/';
+newCrawlModule.fullUrl = 'http://www.tagadajones.com/new/fr/home#/Dates/';
 newCrawlModule.band = new Band();
 newCrawlModule.band.name = 'Tagada Jones';
 newCrawlModule.band.website = 'http://www.tagadajones.com';
 
-newCrawlModule.band.styles = [ "Punk hardcore", "Crossover thrash", "Digital hardcore", "Metalcore" ]
+newCrawlModule.band.styles = [ "Punk hardcore", "Crossover thrash", "Digital hardcore", "Metalcore" ];
 
 // Override the method that assess the web page structure
 newCrawlModule.testDataAcess = function() {
-  winston.info('tagada jones testDataAcess');
-  return true;
+    winston.info('tagada jones testDataAcess');
+    return true;
 };
 
 // Override the method that retrieve the events data
 newCrawlModule.processData = function(window) {
 
- winston.info('tagada jones processDate');
+    winston.info('tagada jones processDate');
 
-  var $ = require('jquery')(window);
-  var dates_table = $('table.dates_list');
-	var rows = $ ('#scroll-content-dates > ul > li');
-	
-  var results = [];
+    var $ = require('jquery')(window);
+	  var rows = $('#scroll-content-dates > ul > li');
 
-  console.log('tagada jones entries: ', rows.length);
-  rows.each (function (index) {
-    var date = $('span.DateDate', this).text ();
-    var fullLocation = $(this).text ();
-    var location = fullLocation.split ('\n')[2].trim ();
-    var venue = fullLocation.split ('\n')[4].trim ();
-    var dateElements = date.split ("/");
-		
-		results.push({date: new Date (dateElements[2], dateElements[1]-1, dateElements[0]).toString('yyyy-MM-dd'), 
-			      location: location,
-			      venue: venue
-			     });
+    var results = [];
 
-  });
+    console.log('tagada jones entries: ', rows.length);
+    rows.each (function (index) {
+        var date = $('span.DateDate', this).text();
+        var fullLocation = $('span.DateDetail', this).text();
 
-  return results;
+        var details = fullLocation.split('\n');
+//        winston.info(details);
+        var location = details[1].trim() + ',' + details[2].trim();
+        var venue = details[3].trim();
+        var dateElements = date.split("/");
+
+		    results.push({date: new Date (dateElements[2], dateElements[1]-1, dateElements[0]).toString('yyyy-MM-dd'),
+			                location: location,
+			                venue: venue
+			               });
+
+    });
+
+    return results;
 };
 
 
 module.exports = {
-  crawlModule: newCrawlModule
+    crawlModule: newCrawlModule
 };
 
 
