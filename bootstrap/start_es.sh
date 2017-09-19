e5a83a0ff6320621c8374c5bf9bd08344125769e
Bhaal22 <muller.john@gmail.com>
2017-09-19 11:14:11 +0200

    update docker usage with official elastic.co docker image

--- bootstrap/start_es.sh
+++ bootstrap/start_es.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-docker run -d -p 9200:9200 -p 9300:9300 -v "/tmp/esdata":/usr/share/elasticsearch/data elasticsearch
+docker run -d -p 9200:9200 -p 9300:9300 -v "/var/whatsclose-data":/usr/share/elasticsearch/data -e "discovery.type=single-node" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:5.6.1
